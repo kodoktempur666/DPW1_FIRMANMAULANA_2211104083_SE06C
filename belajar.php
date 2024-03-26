9cbb629f28aecbdf98a38e8d7df511cbb482e532
Firman Maulana <147988428+ChipiChapaa@users.noreply.github.com>
2024-03-26 08:42:46 +0700

    Membuat program PHP untuk menampilkan bilangan prima 1 - 100

---
+++ belajar.php
@@ -0,0 +1,41 @@
+<?php 
+
+// echo "Saya sedang belajar php";
+// echo "<br>";
+// echo "hello world";
+
+// $a='Saya Sedang belajar PHP';
+// $b=5;
+// print $a;
+// echo $b
+
+
+// const nama= "Kang Daniel";
+// echo nama; // Kang Daniel
+
+// define("nama", "Kang Daniel");
+// echo nama; // Kang Daniel
+
+// $name = "Kang Daniel";
+// $address = '123 Main Street';
+
+// $age = 27;
+// $quantity = 10;
+
+// $a = 10;
+// $b = 5;
+// $c = $a + $b; //penjumlahan
+// $d = $a - $b; //pengurangan
+// $e = $a * $b; //perkalian
+// $f = $a / $b; //pembagian
+// $g = $a % $b; //modulus
+// $h = $a ** $b; //pangkat
+
+function tambah($a, $b) {
+    $total = $a + $b;
+    return $total;
+    }
+
+    $total = tambah(10, 5);
+    echo $total;
+    
